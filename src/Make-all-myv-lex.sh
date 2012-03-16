449ad626dd74c49751e9b9eeb37e845e17d97575
Jack Rueter <rueter.jack@gmail.com>
2012-03-16 08:40:54 +0000

    Comparing with invisible lexica in adv_*, postp_*, conj_* and interj_*
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/kt/myv@55241 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- src/Make-all-myv-lex.sh
+++ src/Make-all-myv-lex.sh
@@ -3,7 +3,11 @@
 
 cat myv-lex.txt \
  adj_myv-lexc.txt \
+ adv_myv-lexc.txt \
+ conj_myv-lexc.txt \
+ interj_myv-lexc.txt \
  noun_myv-lexc.txt \
+ postp_myv-lexc.txt \
  verb_myv-lexc.txt \
  TestLexc-noun.txt \
  noun-myv-morph.txt \
