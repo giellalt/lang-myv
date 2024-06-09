c72e164cebc58fa794f153d75fc702bc9392517a
Jack Rueter <rueter.jack@gmail.com>
2024-06-09 21:06:35 +0300

    Extend conversions for shared-urj

--- src/fst/scripts/urj-Cyrl-myv-conversion.pl
+++ src/fst/scripts/urj-Cyrl-myv-conversion.pl
@@ -21,16 +21,55 @@ while(<>) {
     	s/ PROP-PLC_KUDO/ PROP_KUDO/g ;
         s/(о|а|я|ё|у|ю) CYRL-VOW_SUR/$1 N_KUDO/g ;
         s/(е|ы|и|э) CYRL-VOW_SUR/$1 N_VELE/g ;
-	s/(о|а|я|ё|у|ю)(с|нс|мс|ц|кс) CYRL-SIBILANT_SUR/$1$2 N_OZKS/g ;
-	s/(е|ы|и|э)(с|нс|мс|ц|кс) CYRL-SIBILANT_SUR/$1$2 N_LEVKS/g ;
+        s/(е|ы|и|э) PROP_KUDO/$1 PROP_VELE/g ;
+	s/(о|а|я|ё|у|ю)(|[бвгджзклмнпрстфхцчш]+)(с|нс|мс|ц|кс) CYRL-SIBILANT_SUR/$1$2$3 N_OZKS/g ;
+	s/(е|ы|и|э)(|[бвгджзклмнпрстфхцчш]+)(с|нс|мс|ц|кс) CYRL-SIBILANT_SUR/$1$2$3 N_LEVKS/g ;
+
+	s/(о|а|я|ё|у|ю)(|[бвгджзклмнпрстфхцчш]+)(с|нс|мс|ц|кс) (PROP_KAL|PROP_OSH)/$1$2$3 N_OZKS/g ;
+	s/(е|ы|и|э)(|[бвгджзклмнпрстфхцчш]+)(с|нс|мс|ц|кс) (PROP_KAL|PROP_OSH)/$1$2$3 N_LEVKS/g ;
+	
 	s/(о|а|я|ё|у|ю)(ч|ш|ж) CYRL-SIBILANT_SUR/$1$2 N_OSH/g ;
 	s/(е|ы|и|э)(ч|ш|ж) CYRL-SIBILANT_SUR/$1$2 N_KENKSH/g ;
+	
+	s/(о|а|я|ё|у|ю)(ч|ш|ж) (PROP_KAL|PROP_OSH)/$1$2 PROP_OSH/g ;
+	s/(е|ы|и|э)(ч|ш|ж) (PROP_KAL|PROP_OSH)/$1$2 PROP_KENKSH/g ;
+	
 	s/(о|а|я|ё|у|ю)(д|л|н|р) CYRL-SIBILANT_SUR/$1$2 N_KAL/g ;
 	s/(е|ы|и|э)(д|л|н|р) CYRL-SIBILANT_SUR/$1$2 N_TYL/g ;
+	
+	s/(е|ы|и|э)(|[бвгджзклмнпрстфхцчш]+)(д|л|н|р) PROP_KAL/$1$2$3 PROP_TYL/g ;
+	s/(д|л|н|р)(ь) PROP_KAL/$1$2 PROP_KEL1/g ;
+	
 	s/(о|а|я|ё|у|ю)(т) CYRL-SIBILANT_SUR/$1$2 N_ART/g ;
 	s/(е|ы|и|э)(т) CYRL-SIBILANT_SUR/$1$2 N_KIT/g ;
-	s/(о|а|я|ё|у|ю)(б|в|г|к|м|п|ф|х) CYRL-SIBILANT_SUR/$1$2 N_LAV/g ;
-	s/(е|ы|и|э)(б|в|г|к|м|п|ф|х) CYRL-SIBILANT_SUR/$1$2 N_TEV/g ;
+	s/(ть) CYRL-SIBILANT_SUR/$1 N_GAJT1/g ;
+	
+	s/(о|а|я|ё|у|ю)(|[бвгджзклмнпрстфхцчш]+)(т) PROP_KAL/$1$2$3 PROP_ART/g ;
+	s/(е|ы|и|э)(|[бвгджзклмнпрстфхцчш]+)(т) PROP_KAL/$1$2$3 PROP_KIT/g ;
+	s/(ть) PROP_KAL/$1 PROP_GAJT1/g ;
+	s/(й) PROP_KAL/$1 PROP_PEJ/g ;
+	s/(сь|зь) (PROP_KAL|PROP_OSH)/$1 PROP_ROZ1/g ;
+	
+	s/(о|а|я|ё|у|ю)(|[бвгжкмпфхчш]+)(к) CYRL-SIBILANT_SUR/$1$2$3 N_LAK/g ;
+	s/(е|ы|и|э)(|[бвгжкмпфхчш]+)(к) CYRL-SIBILANT_SUR/$1$2$3 N_VEREK/g ;
+
+	s/(о|а|я|ё|у|ю)(|[бвгжкмпфхчш]+)(к) PROP_KAL/$1$2$3 N_LAK/g ;
+	s/(е|ы|и|э)(|[бвгжкмпфхчш]+)(к) PROP_KAL/$1$2$3 N_VEREK/g ;
+	s/(е|ы|и|э)(б|в|г|м|п|ф|х)(ь) PROP_KAL/$1$2$3 N_OZIM1/g ;
+	s/(о|а|я|ё|у|ю)(б|в|г|м|п|ф|х)(ь) PROP_KAL/$1$2$3 N_RUF1/g ;
+	s/(ж|ч|ш)(ь) PROP_KAL/$1$2 N_RECH1/g ;
+
+	s/(о|а|я|ё|у|ю)(д|з|л|н|р|с|т|ц)(к) CYRL-SIBILANT_SUR/$1$3$2 N_JORK/g ;
+	s/(е|ы|и|э)(д|з|л|н|р|с|т|ц)(к) CYRL-SIBILANT_SUR/$1$2$3 N_OCHERK/g ;
+	
+	s/(о|а|я|ё|у|ю)(д|з|л|н|р|с|т|ц)(к) PROP_KAL/$1$3$2 N_JORK/g ;
+	s/(е|ы|и|э)(д|з|л|н|р|с|т|ц)(к) PROP_KAL/$1$2$3 N_OCHERK/g ;
+
+	s/(о|а|я|ё|у|ю)(б|в|г|м|п|ф|х) CYRL-SIBILANT_SUR/$1$2 N_LAV/g ;
+	s/(е|ы|и|э)(б|в|г|м|п|ф|х) CYRL-SIBILANT_SUR/$1$2 N_TEV/g ;
+	s/(о|а|я|ё|у|ю)(б|в|г|м|п|ф|х) PROP_KAL/$1$2 N_LAV/g ;
+	s/(е|ы|и|э)(б|в|г|м|п|ф|х) PROP_KAL/$1$2 N_TEV/g ;
+	
 #	s/SUND/BERN/g ;
 #	s/HEIM/BERN/g ;
 #	s/NIKOSIIJA/ACCRA/g ;
